50d651ec9d1b4f160fe50024efbaa3249caa19ef
LuisCristovao <36110160+LuisCristovao@users.noreply.github.com>
2020-12-22 15:57:03 +0000

    change post set key
    Now receives json with key value

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ Created on Mon Feb 25 18:12:02 2019
 
 import flask as fl
 from sys import argv
+import json
 
 app = fl.Flask(__name__, static_url_path='')
 
@@ -39,10 +40,13 @@ def getKeyPost():
 	resp.headers['Access-Control-Allow-Origin'] = '*'
 	return resp
     
-@app.route('/setKey/<key>',methods=["POST"])
-def setKeyPost(key):
+@app.route('/setKey',methods=["POST"])
+def setKeyPost():
 	received_values=fl.request.data.decode("ascii")
-	map[key]=received_values
+	json_data=json.loads(received_values)
+	key=json_data["key"]
+	value=json_data["value"]
+	map[key]=value
 	resp = fl.Response("Key {0}:{1} inserted!".format(key,received_values))
 	resp.headers['Access-Control-Allow-Origin'] = '*'
 	return resp
